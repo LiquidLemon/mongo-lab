31334bf65207b02bf3d0f3da60625e729c962ec4
LiquidLemon <liquidlemon@protonmail.com>
2020-12-14 21:12:49 +0100

    Add collection configuration commands

---
+++ collections.js
@@ -0,0 +1,25 @@
+// > use test
+sh.enableSharding("test")
+for (var i = 0; i < 20; i++) {
+  db.a.insert({value: i});
+}
+
+db.createCollection("b")
+db.b.ensureIndex({value: "hashed"})
+sh.shardCollection("test.b", {value: "hashed"})
+for (var i = 0; i < 20; i++) {
+  db.b.insert({value: i});
+}
+
+
+sh.addShardToZone("shard2Set", "Zone2")
+sh.addShardToZone("shard3Set", "Zone3")
+db.createCollection("c")
+db.c.ensureIndex({value: 1})
+sh.updateZoneKeyRange("test.c", {value: MinKey}, {value: 10}, "Zone2")
+sh.updateZoneKeyRange("test.c", {value: 10}, {value: MaxKey}, "Zone3")
+sh.shardCollection("c", {value: 1})
+
+for (var i = 0; i < 20; i++) {
+  db.c.insert({value: i});
+}
